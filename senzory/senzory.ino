9cc06928a02eefd5af90214adae16905a606c394
Jaroslav Lzicar <jarda.lzicar@seznam.cz>
2014-11-12 21:10:57 +0100

    Přidán kód pro odesílání dat na server, screenshot výstupu z php aplikace

--- senzory/senzory.ino
+++ senzory/senzory.ino
@@ -2,6 +2,9 @@
 #include <SFE_BMP180.h>
 #include <Wire.h>
 #include <DHT.h>
+#include <Ethernet.h>
+#include <SPI.h>
+#include <math.h>
 
 #define ALTITUDE 275
 #define INTERVAL 2000
@@ -9,16 +12,19 @@
 
 SFE_BMP180 pressure;
 DHT dht(DHTPIN, DHT22);
+EthernetClient client;
 
-char status;
-double T,P;
-float temp, pres, hum;
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x0F, 0x4B, 0x5F }; 
+char server[] = "192.168.1.10";
+String data;
+int temp, hum, pres;
   
 void setup(){
  
   Serial.begin(9600);
   pressure.begin();
-  dht.begin();  
+  dht.begin(); 
+  Ethernet.begin(mac); 
  
   delay(2000);
 }
@@ -28,40 +34,92 @@ void loop(){
   temp=-999;
   pres=-999;
   
-  // Humidity measurement
-  hum = dht.readHumidity();
-  if(isnan(hum)){ 
-    hum = -999;
+  hum=measureHumidity();
+  temp=measureTemperature();
+  pres=measurePressure(temp);
+  
+  printValues();
+  
+  data =(String)"temp=" + temp + "&pres=" + pres + "&hum=" + hum;
+  
+  sendToServer(server, 80, data);
+      
+  //Wait between measurements
+  delay(INTERVAL); 
+}
+
+//===========================================================================================
+
+int measureHumidity(){
+  int value;
+  value =(int) (dht.readHumidity()*10);
+  if(isnan(value)){ 
+    value = -999;
     Serial.println("error retrieving pressure measurement");
   }
-  
-  // Temperature measurement
+  return value; 
+}
+
+int measureTemperature(){
+  int value = -999;
+  char status;
+  double T;
   status = pressure.startTemperature();
   if (status != 0){
     delay(status);
     status = pressure.getTemperature(T);
     
     if (status != 0){  
-      temp = (float)T;
+      value = (int)(T*100);
     }
     else Serial.println("error retrieving temperature measurement");  
   }
   else Serial.println("error starting temperature measurement");
-  
-  // Pressure measurement
+  return value;
+}
+
+int measurePressure(int temperature){
+  int value = -999;
+  char status;
+  double P;
+  double T= (double)temperature/100.0;
   status = pressure.startPressure(3);
-  if (status != 0 && temp != -999){      
+  if (status != 0 && temperature != -999){      
     delay(status);
     status = pressure.getPressure(P,T);
     
     if (status != 0){
-      pres = (float)pressure.sealevel(P,ALTITUDE); 
+      value = (int)(round(pressure.sealevel(P,ALTITUDE)*10.0)); 
     }
     else Serial.println("error retrieving pressure measurement");   
   }
   else Serial.println("error starting pressure measurement");
+  return value;
+}
 
-  // Print values via serial (debugging only)     
+void sendToServer(char server[], int port, String data){
+  if (client.connect(server,port)) {
+                Serial.println("-> Connected");
+		client.println("POST /arduino/add.php HTTP/1.1"); 
+		client.print("Host: "); 
+                client.println(server);
+		client.println("Content-Type: application/x-www-form-urlencoded"); 
+		client.print("Content-Length: "); 
+		client.println(data.length()); 
+		client.println(); 
+		client.print(data); 
+                Serial.println(data);
+                Serial.println();
+  } 
+  else Serial.println("Connection failed");
+
+  if (client.connected()) { 
+    client.stop();	
+  }
+}
+
+// Print values via serial (debugging only) 
+void printValues(){
   Serial.print("temperature: ");
   Serial.print(temp);
   Serial.print(" C <> ");  
@@ -71,7 +129,4 @@ void loop(){
   Serial.print("humidity: ");
   Serial.print(hum);
   Serial.println(" % ");
-  
-  //Wait between measurements
-  delay(INTERVAL); 
 }
